a66ba19312a72952602356beda7231853882b5a0
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-05-16 14:34:10 +0300

    refactor tests

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -2,8 +2,8 @@ from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
-def test_search_appium():
-    with step('Type search'):
+def test_search():
+    with step('Type search Appium'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
@@ -11,9 +11,9 @@ def test_search_appium():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+        results.first.click()
 
-def test_search_python():
-    with step('Type search'):
+    with step('Type search Python'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Python')
 
@@ -21,3 +21,4 @@ def test_search_python():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Python'))
+        results.first.click()
